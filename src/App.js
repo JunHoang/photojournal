49e14f4eb93caf316186c9bfbe45068464a31739
Dun <kathymaidung@gmail.com>
2020-09-23 13:45:44 +0300

    class-based component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import BannerImage from'./components/BannerImage';
+import PhotoEntry from './components/PhotoEntry';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BannerImage/>
+        <div>
+          <h1>Welcome to My Photojournal</h1>
+          <h3>Dung Hoang</h3>
+          <PhotoEntry src="berlin.jpg" caption="The Heart of Europe" location="Berlin, Germany"/>
+        </div>
       </header>
     </div>
   );
