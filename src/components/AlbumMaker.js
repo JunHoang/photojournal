f3e7e3df74c4c95b8913d735784aa43850c6d36e
Dun <kathymaidung@gmail.com>
2020-09-23 14:34:03 +0300

    generate content with json

--- src/components/AlbumMaker.js
+++ src/components/AlbumMaker.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PhotoEntry from './PhotoEntry';
+import entries from './data/photo-entries';
 
 class AlbumMaker extends React.Component {
     render() {
+        console.log(entries);
+        
         return(
             <div className="flex-container">
-                <PhotoEntry src="berlin.jpg" location="Berlin, German" caption="Heart of Europe"/>
-                <PhotoEntry src="beverly-hills.jpg" location="Beverly Hills, USA" caption="Southern California" />
-                <PhotoEntry src="maple-leaf.jpg" location="Toronto, Canada" caption="Autumn" />
-                <PhotoEntry src="wings.jpg" location="San Francisco, USA" caption="Wanderlust" />
-                <PhotoEntry src="hawaii.jpg" location="Hona, Hawaii, USA" caption="Deep Thought" />
-                <PhotoEntry src="london.jpg" location="London, UK" caption="Trafalga Square" />
+                {
+                    entries.itemlist.map((photo) => {
+                        return(
+                            <PhotoEntry key={photo.src} src={photo.src} location={photo.location} caption={photo.caption} />
+                        )
+                    })
+                }
+                {/* <PhotoEntry src="berlin.jpg" location="Berlin, German" caption="Heart of Europe"/> */}
             </div>
         )
     }
