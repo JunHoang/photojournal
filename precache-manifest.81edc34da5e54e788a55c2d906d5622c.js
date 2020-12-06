4c7d08557cbd6a7b96d34d59f1209d31b5fbd9b7
DungHoang <kathymaidung@gmail.com>
2020-12-06 16:00:51 +0200

    Updates

---
+++ precache-manifest.81edc34da5e54e788a55c2d906d5622c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e1b7165f2aff685bf1d199750599e7",
+    "url": "/photojournal/index.html"
+  },
+  {
+    "revision": "09ec9f6ea57bdf23870e",
+    "url": "/photojournal/static/css/main.33540dd8.chunk.css"
+  },
+  {
+    "revision": "fd3a814f9d2c4ca62d79",
+    "url": "/photojournal/static/js/2.43be6d34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photojournal/static/js/2.43be6d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ec9f6ea57bdf23870e",
+    "url": "/photojournal/static/js/main.6a04c80b.chunk.js"
+  },
+  {
+    "revision": "b04db2344f2327056f45",
+    "url": "/photojournal/static/js/runtime-main.d28718b0.js"
+  }
+]);
